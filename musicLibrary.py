91d8b3989ce44e37fea0b54f1027430892dba0b7
SAVINAV <vectorarchitect0@gmail.com>
2026-02-20 02:21:07 +0530

    Added custom MusicLibrary.

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "shape of you":"https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "blinding lights":"https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    "dance monkey":"https://www.youtube.com/watch?v=q0hyYWKXF0Q",
+    "rockabye":"https://www.youtube.com/watch?v=papuvlVeZg8"
+}
